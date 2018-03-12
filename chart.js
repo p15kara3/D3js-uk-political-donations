39f075eb256207067cb824ebed71b3b21b49ecdf
p15kara3 <p15kara3@ionio.gr>
2018-03-12 19:45:23 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -270,22 +270,25 @@ function moveToFunds(alpha) {
 }
 function moveToAmounts(alpha) {
 	return function(d) {
-		var centreY = svgCentre.y;
+		//var centreY = svgCentre.y;
 		if (d.value <= 25001) {
-				centreX = svgCentre.x + 250;
-			} else if (d.value <= 50001) {
-			        centreX = svgCentre.x + 200;
-			} else if (d.value <= 100001) {
-				centreX = svgCentre.x + 175;
-			} else  if (d.value <= 500001) {
-				centreX = svgCentre.x + 150;
-			} else  if (d.value <= 1000001) {
-				centreX = svgCentre.x +80;
-			} else if (d.value<= maxVal) {	
-				centreX =svgCentre.x;
-			} else {
 				centreX = svgCentre.x;
-			}
+				centreY = svgCentre.y + 50;
+			} else if (d.value <= 50001) {
+			        centreX = svgCentre.x + 250;
+				centreY= svgCentre.y + 60;
+			//} else if (d.value <= 100001) {
+			//	centreX = svgCentre.x + 175;
+			//} else  if (d.value <= 500001) {
+			//	centreX = svgCentre.x + 150;
+			//} else  if (d.value <= 1000001) {
+			//	centreX = svgCentre.x +80;
+			//} else if (d.value<= maxVal) {	
+			//	centreX =svgCentre.x;
+			//} else {
+			//	centreX = svgCentre.x;
+			//}
+
 
 		d.x += (centreX - d.x) * (brake + 0.06) * alpha * 1.2;
 		d.y += (centreY - d.y) * (brake + 0.06) * alpha * 1.2;
