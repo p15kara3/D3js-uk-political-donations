b76cd4dfe75523a98c0912b8def530715f822f43
p15kara3 <p15kara3@ionio.gr>
2018-03-12 02:00:36 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -79,7 +79,7 @@ function transition(name) {
 		$("#view-source-type").fadeIn(1000);
 		return fundsType();
 	}
-	if (name === "group-by-amount-of-donation") {
+	if (name === "group-by-amount") {
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
